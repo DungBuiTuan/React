739f45a25d188c88bc1e58b1df2fbaa548e12dd2
Dung <TuanDung@gmail.com>
2023-06-23 12:18:46 +0700

    Redux Actions

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,10 @@
-import React from 'react';
+/* eslint-disable react/jsx-pascal-case */
+import React,{Component} from 'react';
 import { Card, CardImg, CardText, CardBody,
-    CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+    CardTitle, Breadcrumb, BreadcrumbItem, Label,
+    Modal, ModalHeader, ModalBody, Button, Row, Col  } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Control, LocalForm } from 'react-redux-form';
 
 
 
@@ -24,7 +27,8 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments }) {
+// function RenderComments({ comments }) {
+    function RenderComments({comments, addComment, dishId}) {
     if (comments != null) {
         const comment = comments.map((comment) => {
             return (
@@ -44,6 +48,7 @@ function RenderComments({ comments }) {
                     </p>
                 </li>
             </div>
+            <CommentForm dishId={dishId} addComment={addComment} />
                 </div>
             );
         });
@@ -63,6 +68,70 @@ function RenderComments({ comments }) {
             <div></div>
         )
     }
+}
+
+class CommentForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        
+        this.state = {
+          isNavOpen: false,
+          isModalOpen: false
+        };
+    }
+
+    toggleModal() {
+        this.setState({
+          isModalOpen: !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit(values) {
+        this.toggleModal();
+        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+    }
+
+    render() {
+        return(
+        <div>
+            <Button outline onClick={this.toggleModal}><span className="fa fa-pencil fa-lg"></span> Submit Comment</Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+            <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+            <ModalBody>
+                <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Row className="form-group">
+                        <Col>
+                        <Label htmlFor="rating">Rating</Label>
+                        <Control.select model=".rating" id="rating" className="form-control">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select>
+                        </Col>
+                    </Row>
+                    <Row className="form-group">
+                        <Col>
+                        <Label htmlFor="comment">Comment</Label>
+                        <Control.textarea model=".comment" id="comment"
+                                    rows="6" className="form-control" />
+                        </Col>
+                    </Row>
+                    <Button type="submit" className="bg-primary">
+                        Submit
+                    </Button>
+                </LocalForm>
+            </ModalBody>
+           </Modal>
+        </div>
+        );
+    }
+
 }
 
 const DishDetail = (props) => {
@@ -90,7 +159,9 @@ const DishDetail = (props) => {
                 <RenderDish dish={props.dish} />
             </div>
             <div className="col-12 col-md-5 m-1">
-                <RenderComments comments={props.comments} />
+            <RenderComments comments={props.comments}
+            addComment={props.addComment}
+            dishId={props.dish.id}/>    
             </div>
         </div>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -10,7 +10,7 @@ import About from './AboutComponent';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
 import { LEADERS } from '../shared/leaders';
-
+import { addComment } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 
@@ -22,6 +22,13 @@ const mapStateToProps = state => {
     leaders: state.leaders
   }
 }
+
+
+const mapDispatchToProps = dispatch => ({
+  
+  addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+
+});
 
 
 //UseParsms can't not import
@@ -58,8 +65,10 @@ class Main extends Component {
 
     const DishWithId = ({match}) => {
       return(
-          <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-            comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+        comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+        addComment={this.props.addComment}/>
+
       );
     };
 
@@ -82,5 +91,5 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
 
